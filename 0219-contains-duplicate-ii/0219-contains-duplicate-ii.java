5ef094be490a3dd36f56c590c80ce1ce8dc1e0b3
Rivka Gvirer <rgv1198@gmail.com>
2025-01-10 01:45:16 +0200

    Time: 18 ms (82.54%), Space: 58.6 MB (10.59%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -5,7 +5,7 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             if (map.containsKey(nums[i])) {
                 int j = map.get(nums[i]);
-                if (Math.abs(i - j) <= k) {
+                if (i - j <= k) {
                     return true;
                 }
             }
