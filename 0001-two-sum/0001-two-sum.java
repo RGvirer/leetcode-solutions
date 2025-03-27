5dc164bf3928460b84da1beea84e053f5c3616c7
Rivka Gvirer <rgv1198@gmail.com>
2025-03-28 00:25:49 +0200

    Time: 46 ms (19.24%), Space: 44.7 MB (94.3%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,16 +1,12 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] output=new int[2];
-        int sum=0;
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target){
-                        output[0]=i;
-                        output[1]=j;
-                        return output;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) { // j צריך להתחיל מ-i+1
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
                 }
             }
         }
-        return output;
+        return new int[]{-1, -1}; // ערך המציין שלא נמצא פתרון
     }
 }
